b7310e86a13066b9116fd4b7e8917ace4c92afd2
Christian Fischer-Pauzenberger <emailcfp@gmail.com>
2018-10-21 13:58:49 +0200

    Initial commit.

--- server.R
+++ server.R
@@ -14,67 +14,244 @@ $('#' + boxid).closest('.box').find('[data-widget=collapse]').click();
 }
 "
 
-sqlite <- dbConnect(SQLite(), "db.sqlite")
+#sqlite <- dbConnect(SQLite(), "db.sqlite")
+sqlite <- dbConnect(SQLite(), "options.sqlite")
+
+Nd20 <- 0
+Time_to_maturity_0 <- 0
+
+Nd1_old <- 0
+A_Value_old <- 0
+L_Value_old <- 0
+
+
+date_act <- 0
+Nd1_act <- 0
+A_Value_act <- 0
+L_Value_act <- 0
 
 server <- function(input, output, session) {
+
   observeEvent(input$ab_Initial_Pricing, {
-    js$collapse("box_Do")
+    js$collapse("box_Measure")
     hide(id = "box_Initial_Pricing", anim = FALSE)
     
-    temp_db_Stock_Derivative_Static <-
+    temp_db_derivative_instrument_master <-
       cbind.data.frame(
-        input$ti_Type_Of_Stock_Derivative,
         input$ti_Stock_ISIN,
+        input$ti_Type_Of_Stock_Derivative,
         input$ti_Exercise_Or_Forward_Price,
+        
         as.character(input$ti_Contracting_Date),
         as.character(input$ti_Expiration_Date),
+        input$ti_Contracting_Security_Price,
+        
         input$ti_Contract_Size,
         input$ti_Number_Of_Contracts,
         input$ti_Stock_Volatility,
+        
         input$ti_Interest_Rate,
         input$ti_Mark_To_Model
       )
-    names(temp_db_Stock_Derivative_Static) <-
+    
+    names(temp_db_derivative_instrument_master) <-
       c(
-        "Type_Of_Stock_Derivative",
-        "Stock_ISIN",
-        "Exercise_Or_Forward_Price",
-        "Contracting_Date",
-        "Expiration_Date",
-        "Contract_Size",
-        "Number_Of_Contracts",
-        "Stock_Volatility",
-        "Interest_Rate",
-        "Mark_To_Model"
+        "isin", 
+        "type_of_stock_derivative",
+        "exercise_or_forward_price", 
+        "contracting_date", 
+        "expiration_date", 
+        "contracting_security_price", 
+        "contracting_size", 
+        "number_of_contracts", 
+        "volatility", 
+        "interest_rate", 
+        "mark_to_model"
       )
+    
     dbWriteTable(sqlite,
-                 "Stock_Derivative_Static",
-                 temp_db_Stock_Derivative_Static,
+                 "derivative_instrument_master",
+                 temp_db_derivative_instrument_master,
                  append = TRUE)
+    
+  
+    
+    P <- as.numeric(input$ti_Contracting_Security_Price) #contracting_security_price
+    X <- as.numeric(input$ti_Exercise_Or_Forward_Price) #exercise_or_forward_price
+    r <- as.numeric(input$ti_Interest_Rate)/100 #interest_rate
+    sigma <- as.numeric(input$ti_Stock_Volatility)/100 # volatility
+    
+    #cat(file=stderr(), "p: ", P, "\n")
+    #cat(file=stderr(), "X: ", X, "\n")
+    #cat(file=stderr(), "r: ", r, "\n")
+    #cat(file=stderr(), "sigma: ", sigma, "\n")
+
+    Time_to_maturity <- as.numeric(difftime(toString(isolate(input$ti_Expiration_Date)),
+                                                  toString(isolate(input$ti_Contracting_Date))))/365
+  
+    Time_to_maturity_0 <<- Time_to_maturity
+    #cat(file=stderr(), "contdate: ", toString(isolate(input$ti_Contracting_Date)), "\n")
+    #cat(file=stderr(), "expdate: ", toString(isolate(input$ti_Expiration_Date)), "\n")
+    #cat(file=stderr(), "TtM: ", Time_to_maturity, "\n")
+    
+    d1 <- (log(P/X) + (r + sigma*sigma/2) * Time_to_maturity) /( sigma*sqrt(Time_to_maturity))
+    Nd1 <- pnorm(d1)
+    #cat(file=stderr(), "Nd1: ", Nd1, "\n")
+    
+    d2 <- d1 - sigma * sqrt(Time_to_maturity)
+    Nd2 <- pnorm(d2)
+    
+    Nd20 <<- Nd2
+    #cat(file=stderr(), "Nd2: ", Nd2, "\n")
+    
+    Asset <- P * Nd1
+    Liability <- -X*exp(-r*Time_to_maturity)*Nd2
+    
+    #cat(file=stderr(), "Asset: ", Asset, "\n")
+    #cat(file=stderr(), "Liability: ", Liability, "\n")
+    
+    Nd1_old <<- Nd1
+    A_Value_old <<- Asset
+    L_Value_old <<- Liability
+    
+    #database
+    temp_db_derivative_instrument_transactional <-
+      cbind.data.frame(
+        1,
+        1,
+        as.character(input$ti_Contracting_Date),
+        Asset + Liability
+      )
+    
+    names(temp_db_derivative_instrument_transactional) <-
+      c(
+        "derivate_instrument_master_id",
+        "event_id",
+        "timestamp_",
+        "fair_value"
+      )
+    
+    dbWriteTable(sqlite,
+                 "derivative_instrument_transactional",
+                 temp_db_derivative_instrument_transactional,
+                 append = TRUE)
+    
+    ##Risky income
+    temp_db_economic_resource_risky_income <-
+      cbind.data.frame(
+        1,
+        1,
+        Nd1,
+        Asset,
+        0
+      )
+    
+    names(temp_db_economic_resource_risky_income) <-
+      c(
+        "derivative_instrument_transactional_id",
+        "resource_id",
+        "nd1t",
+        "value",
+        "asset_or_liability"
+      )
+    
+    dbWriteTable(sqlite,
+                 "economic_resource_risky_income",
+                 temp_db_economic_resource_risky_income,
+                 append = TRUE)
+    
+    ##Fixed income
+    temp_db_economic_resource_fixed_income <-
+      cbind.data.frame(
+        1,
+        1,
+        Liability,
+        1
+      )
+    
+    names(temp_db_economic_resource_fixed_income) <-
+      c(
+        "derivative_instrument_transactional_id", 
+        "resource_id", 
+        "present_value", 
+        "asset_or_liability"
+      )
+    
+    dbWriteTable(sqlite,
+                 "economic_resource_fixed_income",
+                 temp_db_economic_resource_fixed_income,
+                 append = TRUE)
+  
   })
-  observeEvent(input$button_Do, {
-    temp_db_Stock_Pricing_Dynamic <-
+    
+  #event Measure  
+  observeEvent(input$button_Measure, {
+    
+    temp_db_financial_security_pricing_transactional <-
       cbind.data.frame(
         input$ti_Stock_ISIN,
-        input$ti_Do_Stock_Price,
-        as.character(input$ti_Do_timestamp)
+        as.character(input$ti_Measure_timestamp),
+        input$ti_Measure_Stock_Price
       )
-    names(temp_db_Stock_Pricing_Dynamic) <-
-      c("Stock_ISIN",
-        "Stock_Price",
-        "timestamp")
+    names(temp_db_financial_security_pricing_transactional) <-
+      c("isin",
+        "timestamp_",
+        "price")
     dbWriteTable(sqlite,
-                 "Stock_Pricing_Dynamic",
-                 temp_db_Stock_Pricing_Dynamic,
+                 "financial_security_pricing_transactional",
+                 temp_db_financial_security_pricing_transactional,
                  append = TRUE)
     
     
     js$collapse("box_Plan")
   })
   
+  
+  
+  
   observeEvent(input$button_Plan, {
     
-    output$to_Plan <- renderText("N(d1) = 1")
+    
+    resultset = dbGetQuery(sqlite, "select timestamp_, price from financial_security_pricing_transactional order by timestamp_ desc limit 1")
+    dbDisconnect(sqlite)
+    P = as.numeric(resultset$price)
+
+    X <- as.numeric(input$ti_Exercise_Or_Forward_Price) #exercise_or_forward_price
+    r <- as.numeric(input$ti_Interest_Rate)/100 #interest_rate
+    sigma <- as.numeric(input$ti_Stock_Volatility)/100 # volatility
+    
+    cat(file=stderr(), "p: ", P, "\n")
+    cat(file=stderr(), "X: ", X, "\n")
+    cat(file=stderr(), "r: ", r, "\n")
+    cat(file=stderr(), "sigma: ", sigma, "\n")
+    
+    Time_to_maturity <- as.numeric(difftime(toString(isolate(input$ti_Expiration_Date)),
+                                            toString(resultset$timestamp_)))/365
+    
+    cat(file=stderr(), "contdate: ", toString(isolate(input$ti_Contracting_Date)), "\n")
+    cat(file=stderr(), "expdate: ", toString(isolate(input$ti_Expiration_Date)), "\n")
+    cat(file=stderr(), "TtM: ", Time_to_maturity, "\n")
+    
+    d1 <- (log(P/X) + (r + sigma*sigma/2) * Time_to_maturity) /( sigma*sqrt(Time_to_maturity))
+    Nd1 <- pnorm(d1)
+    cat(file=stderr(), "Nd1: ", Nd1, "\n")
+
+    Nd1_act <<- Nd1
+    date_act <<- toString(resultset$timestamp_)
+    
+    cat(file=stderr(), "date_act", date_act, "\n")
+    cat(file=stderr(), "d1, t: ", d1, "\n")
+    cat(file=stderr(), "Nd1, t: ", Nd1, "\n")
+    
+    Asset <- P * Nd1
+    Liability <- L_Value_old * exp (r*(Time_to_maturity_0-Time_to_maturity))
+
+    A_Value_act <<- Asset
+    L_Value_act <<- Liability - (Nd1_act - Nd1_old)*P
+
+    
+    
+    output$to_Plan <- renderText(paste("N(d1) = ", Nd1))
     js$collapse("box_Check")
   })
   
@@ -82,16 +259,38 @@ server <- function(input, output, session) {
   
   
   observeEvent(input$button_Check, {
-    output$to_Check <- renderText("Delta N(d1) = 0")
+    
+    
+    cat(file=stderr(), "Nd1, t-1: ", Nd1_old, "\n")
+    cat(file=stderr(), "Delta Nd1, t: ", Nd1_act - Nd1_old, "\n")
+
+    
+    output$to_Check <- renderText(paste("Delta N(d1) = ", Nd1_act - Nd1_old))
     js$collapse("box_Act")
   })
   
   observeEvent(input$button_Act, {
-    output$to_Act <- renderText("Forward: No action possible")
-    v$doCalcAndPlot <- input$button_Act #CalcAndPlot
+    
+
+    
+    cat(file=stderr(), "Asset value after transaction: ", A_Value_act, "\n")
+    cat(file=stderr(), "Liability value after transaction: ", L_Value_act, "\n")
+    cat(file=stderr(), "Portfolio value after transaction: ", +A_Value_act+L_Value_act, "\n")
+    
+    cat(file=stderr(), "as.character(date_act) ", as.character(date_act), "\n")
+    cat(file=stderr(), "(A_Value_act+L_Value_act) ", (A_Value_act+L_Value_act), "\n")
+    cat(file=stderr(), "Nd1_act ", Nd1_act, "\n")
+    cat(file=stderr(), "A_Value_act ", A_Value_act, "\n")
+    cat(file=stderr(), "L_Value_act ", L_Value_act, "\n")
+    
+    
+   output$to_Act <- renderText("Rebalancing executed!")
+  #  v$doCalcAndPlot <- input$button_Act #CalcAndPlot
   })
   
   observeEvent(input$button_Act_Continue, {
+    
+    
     js$collapse("box_Act")
     js$collapse("box_Plan")
     js$collapse("box_Check")
@@ -103,14 +302,14 @@ server <- function(input, output, session) {
   })
   
   observeEvent(input$reset_db, {
-    dbSendStatement(sqlite, "DELETE from Stock_Derivative_Static")
-    dbSendStatement(sqlite, "DELETE from Stock_Pricing_Dynamic")
-    dbSendStatement(sqlite, "DELETE from Derivative_Instrument_Dynamic")
-    dbSendStatement(sqlite, "DELETE from Economic_Resource_Risky_Income")
-    dbSendStatement(sqlite, "DELETE from Economic_Resource_Fixed_Income")
-    dbSendStatement(sqlite, "DELETE from Asset")
-    dbSendStatement(sqlite, "DELETE from Liability")
-    dbSendStatement(sqlite, "DELETE from Off_Balance")
+    dbSendStatement(sqlite, "DELETE from derivative_instrument_master")
+    dbSendStatement(sqlite, "DELETE from economic_resource_fixed_income")
+    dbSendStatement(sqlite, "DELETE from economic_resource_risky_income")
+    dbSendStatement(sqlite, "DELETE from derivative_instrument_transactional")
+    dbSendStatement(sqlite, "DELETE from financial_security_pricing_transactional")
+    #dbSendStatement(sqlite, "DELETE from Asset")
+    #dbSendStatement(sqlite, "DELETE from Liability")
+    #dbSendStatement(sqlite, "DELETE from Off_Balance")
   })
   
 
--- ui.R
+++ ui.R
@@ -17,21 +17,13 @@ ui <- dashboardPage(
   
   dashboardSidebar(sidebarMenu(
     menuItem(
-      "Forward Pricing",
-      tabName = "forwardpricing",
-      icon = icon("balance-scale")
-    ),
-    menuItem(
       "Option Pricing",
       tabName = "optionpricing",
-      badgeColor = "red",
-      badgeLabel = "help!",
-      icon = icon("road")
+      icon = icon("balance-scale")
     ),
-    menuItem("Table Explorer",
+    menuItem("Database explorer",
              tabName = "tableexplorer",
              icon = icon("gear"))
-    
     
   ),
   actionButton("reset_db", "Reset Database")),
@@ -43,7 +35,7 @@ ui <- dashboardPage(
     
     tabItems(
       tabItem(
-        tabName = "forwardpricing",
+        tabName = "optionpricing",
         h2("Forward Contracting"),
         fluidRow(
           box(
@@ -57,45 +49,47 @@ ui <- dashboardPage(
             #actionButton("initialReadFile", "Load initial values"),
             column(
               3,
-              textInput("ti_Type_Of_Stock_Derivative", "Type Of Stock Derivative", "0"),
               textInput("ti_Stock_ISIN", "Stock ISIN", "AT0001"),
-              textInput("ti_Contract_Size", "Contract Size", "1")
+              textInput("ti_Type_Of_Stock_Derivative", "Type Of Stock Derivative", "1"),
+              textInput("ti_Exercise_Or_Forward_Price", "Exercise Or Forward Price", "100")
             ),
             
             column(
               3,
-              textInput("ti_Number_Of_Contracts", "Number Of Contracts", "1"),
-              textInput("ti_Exercise_Or_Forward_Price", "Exercise Or Forward Price", "100"),
+              
               dateInput(
                 "ti_Contracting_Date",
                 "Contracting Date",
                 value = "2020-01-01",
                 min = "2020-01-01"
-              )
-            ),
-            
-            column(
-              3,
+              ),
               dateInput(
                 "ti_Expiration_Date",
                 "Expiration Date",
                 value = "2020-12-31",
                 min = "2020-01-01"
               ),
-              textInput("ti_Interest_Rate", "Interest Rate in %", "4"),
-              textInput("ti_Stock_Volatility", "Stock Volatility in %", "0")
+              textInput("ti_Contracting_Security_Price", "Contracting Security Price", "100")
+            ),
+            
+            column(
+              3,
+              textInput("ti_Contract_Size", "Contract Size", "1"),
+              textInput("ti_Number_Of_Contracts", "Number Of Contracts", "1"),
+              textInput("ti_Stock_Volatility", "Stock Volatility in %", "20")
                    ),
             
             column(
               3,
+              textInput("ti_Interest_Rate", "Interest Rate in %", "5"),
               textInput("ti_Mark_To_Model", "Mark To Model", "1"),
               actionButton("ab_Initial_Pricing", "Finish Initial Pricing")
             )
             ),
           
           box(
-            id = "box_Do",
-            title = "First Step (Do)",
+            id = "box_Measure",
+            title = "First Step (Measure)",
             width = 3,
             align = "center",
             status = "primary",
@@ -105,17 +99,17 @@ ui <- dashboardPage(
             #  title = "Subsequent Pricings",width = 4, status = "primary", solidHeader = TRUE, collapsible = TRUE,
             
             dateInput(
-              "ti_Do_timestamp",
+              "ti_Measure_timestamp",
               "Date",
               value = "2020-01-01",
               min = "2020-01-01"
             ),
             textInput(
-              "ti_Do_Stock_Price",
+              "ti_Measure_Stock_Price",
               "Stock Price",
               value = 100
             ),
-            actionButton("button_Do", "Do")
+            actionButton("button_Measure", "Measure")
             
             
           ),
